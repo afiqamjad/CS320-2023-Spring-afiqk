ff0f7f419c93c0ba5322bc487c259ab876dd6e6c
afiqamjad <afiqk@bu.edu>
2023-02-09 15:10:28 -0500

    Finalised solution for assign02_01.py v2.

--- assigns/02/MySolution/assign02_01.py
+++ assigns/02/MySolution/assign02_01.py
@@ -12,3 +12,41 @@ print("[import ./../assign02.py] is done!")
 # mylist_reverse (see list_reverse in assign02.sml)
 #
 ####################################################
+# fun
+# list_append
+# (xs: 'a list, ys: 'a list): 'a list =
+# (
+# case xs of
+#   nil => ys
+# | x1 :: xs => x1 :: list_append(xs, ys)
+# )
+
+# (* ****** ****** *)
+
+def mylist_append(bs, cs):
+    if mylist_nilq(bs):
+        return cs
+    return mylist_cons(mylist_cons.get_cons1(bs),mylist_append(mylist_cons.get_cons2(bs), cs))
+
+# fun
+# list_rappend
+# (xs: 'a list, ys: 'a list): 'a list =
+# (
+# case xs of
+#   nil => ys
+# | x1 :: xs => list_rappend(xs, x1 :: ys)
+# )
+
+def mylist_rappend(bs, cs):
+    if mylist_nilq(bs):
+        return cs
+    return mylist_rappend(mylist_cons.get_cons2(bs), mylist_cons(mylist_cons.get_cons1(bs), cs))
+
+# (* ****** ****** *)
+# fun
+# list_reverse
+# (xs: 'a list): 'a list = list_rappend(xs, [])
+# (* ****** ****** *)
+
+def mylist_reverse(bs):
+    return mylist_rappend(bs, mylist_nil)
