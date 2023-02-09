e55e90d35aec7e573875e70478d4095cb9b68796
afiqamjad <afiqk@bu.edu>
2023-02-09 17:06:04 -0500

    Finalised assign02-02.

--- assigns/02/MySolution/assign02_02.py
+++ assigns/02/MySolution/assign02_02.py
@@ -2,6 +2,7 @@
 import sys
 sys.path.append('..')
 from assign02 import *
+from assign02_01 import *
 ####################################################
 print("[import ./../assign02.py] is done!")
 ####################################################
@@ -10,3 +11,60 @@ print("[import ./../assign02.py] is done!")
 # mylist_quicksort (see list_quicksort in assign02.sml)
 #
 ####################################################
+
+# fun
+# list_quicksort
+# (xs: int list): int list =
+# let
+    # (* ****** ****** *)
+    # fun
+    # qsort(xs: int list): int list =
+    # (
+    # case xs of
+    # nil => nil
+    # |
+    # x1 :: xs =>
+    # let
+        # val (ys, zs) = qpart(xs, x1)
+        # val ys = qsort(ys) and zs = qsort(zs)
+    # in
+        #   ys @ [x1] @ zs
+    # end
+    # )    #and
+    # qpart
+    # (xs: int list, p0: int): int list * int list =
+    # (
+    # case xs of
+    # nil => (nil, nil)
+    # |
+    # x1 :: xs =>
+    # let
+        # val (ys, zs) = qpart(xs, p0)
+        # in
+        #   if x1 <= p0 then (x1 :: ys, zs) else (ys, x1 :: zs)
+    # end
+    # )
+    # (* ****** ****** *)
+    # in
+    #   qsort(xs)
+# end (* end-of-[list_quicksort]: let *) 
+    
+
+def qpart(xs, p0):
+    if mylist_nilq(xs):
+        return mylist_nil(), mylist_nil()
+    ys, zs = qpart(mylist_cons.get_cons2(xs), p0)
+    if mylist_cons.get_cons1(xs) <= p0:
+        return mylist_cons(mylist_cons.get_cons1(xs), ys), zs
+    return ys, mylist_cons(mylist_cons.get_cons1(xs), zs)
+
+def qsort(xs):
+    if mylist_nilq(xs):
+        return mylist_nil()
+    ys, zs = qpart(mylist_cons.get_cons2(xs), mylist_cons.get_cons1(xs))
+    ys = qsort(ys)
+    zs = qsort(zs)
+    return mylist_append(mylist_append(ys, mylist_cons(mylist_cons.get_cons1(xs), mylist_nil())), zs)
+
+def mylist_quicksort(xs):
+    return qsort(xs)
